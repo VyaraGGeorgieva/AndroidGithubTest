69ef59092918c3f9c971b845fecaddc0896f6ac9
VyaraGGeorgieva <viara.georgieva.g@gmail.com>
2016-06-09 21:07:07 +0300

    Modified main Activity Added new file Student.java

--- app/src/main/java/com/georgieva/vyara/androidgithubintegration/MainActivity.java
+++ app/src/main/java/com/georgieva/vyara/androidgithubintegration/MainActivity.java
@@ -9,5 +9,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        print();
     }
+    public void print(){
+        System.out.println("First change");
+    }
+
 }
---
+++ app/src/main/java/com/georgieva/vyara/androidgithubintegration/Student.java
@@ -0,0 +1,7 @@
+package com.georgieva.vyara.androidgithubintegration;
+
+/**
+ * Created by Vyara on 09-Jun-16.
+ */
+public class Student {
+}
